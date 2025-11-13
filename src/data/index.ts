a2373b07ea75abf98a0d0be13ef02f747c5fbcb3
Kevin Reynaldo Laurens <kevin.laurens@indicocompany.com>
2025-11-13 11:32:15 +0700

    feat: add money changer and auto repair data imports to facilitiesData

--- src/data/index.ts
+++ src/data/index.ts
@@ -3,6 +3,8 @@ import hospitalData from "./hospital.json";
 import pharmacyData from "./pharmacy.json";
 import gasStationData from "./gasStation.json";
 import restaurantData from "./restaurant.json";
+import moneyChangerData from "./moneyChanger.json";
+import autoRepairData from "./autoRepair.json";
 
 export const facilitiesData = {
   atm: atmData,
@@ -10,6 +12,8 @@ export const facilitiesData = {
   pharmacy: pharmacyData,
   gasStation: gasStationData,
   restaurant: restaurantData,
+  moneyChanger: moneyChangerData,
+  autoRepair: autoRepairData,
 };
 
 export default facilitiesData;
